82454dc22bca0dc4a6323ad7f2ec15ae529db16d
its-babita <bbhatta2055@gmail.com>
2022-12-21 11:37:11 +0545

    added contact form with list of contact

---
+++ src/Components/ContactCard.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import user from "../Assets/Images/user.jpeg"
+
+const ContactCard = (props) => {
+    //destructuring
+    const {id,name,email}=props.contact
+  return (
+      <div className="item">
+      <img className='ui avatar image' src={user} alt="user" />
+            <div className="content">
+              <div className="header">{name}</div>
+              <div>{email}</div>
+            </div>
+            <div style={{marginLeft:"600px",marginBottom:"10px"}}>
+            <i style={{color:"red",fontSize:25}} className="trash alternate outline icon"></i>
+            </div>
+          </div>
+  )
+}
+
+export default ContactCard
